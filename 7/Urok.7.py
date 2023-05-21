2571802c261984e9c182c337a926d0713a2db09e
Kirill <kirillkarppekin11532@gmail.com>
2023-05-21 22:32:42 +0300

    first commit

---
+++ 7/Urok.7.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name, fruit = "Kirill", "bananas"
+    x, y = 5, 6 
+    template_context = dict(name=name, fruit = fruit, x =x, y= y)
+    return render_template('Urok7.html', **template_context)
+
+@app.route('/html')
+def index_html():
+    return render_template('Urok7.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
